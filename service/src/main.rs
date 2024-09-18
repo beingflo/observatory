69b740eac4444afbf8f046a720377047841daa20
Florian Marending <florian@marending.dev>
2024-09-18 22:04:14 +0200

    [service] First endpoints

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,3 +1,132 @@
-fn main() {
-    println!("Hello, world!");
+use std::{sync::Arc, time::Duration};
+
+use axum::{
+    extract::State,
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+};
+use chrono::Utc;
+use duckdb::{params, Connection};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use tokio::{runtime::Handle, sync::Mutex, task};
+
+type StateType = (Arc<Mutex<Connection>>, Arc<Mutex<Vec<Data>>>);
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let conn = Arc::new(Mutex::new(Connection::open("./db.duckdb")?));
+    let buffer = Arc::new(Mutex::new(Vec::new()));
+
+    conn.lock().await.execute_batch(
+        r"CREATE TABLE IF NOT EXISTS data (
+            timestamp TIMESTAMP NOT NULL,
+            bucket TEXT NOT NULL,
+            payload JSON NOT NULL
+          );
+        ",
+    )?;
+
+    println!("Created table");
+
+    fn task(conn: Arc<Mutex<Connection>>, buffer: Arc<Mutex<Vec<Data>>>) {
+        let mut interval = tokio::time::interval(Duration::from_millis(1000));
+        loop {
+            let handle = Handle::current();
+            handle.block_on(interval.tick());
+
+            let mut buffer = handle.block_on(buffer.lock());
+
+            if buffer.len() < 1 {
+                continue;
+            }
+
+            let buffer_len = buffer.len();
+            let mut buffer_local: Vec<Data> =
+                buffer.drain(0..std::cmp::min(1000, buffer_len)).collect();
+
+            if buffer.len() == 0 {
+                buffer.shrink_to_fit();
+            }
+            // Free up lock
+            drop(buffer);
+
+            let conn = handle.block_on(conn.lock());
+            conn.execute_batch("BEGIN TRANSACTION").unwrap();
+            let mut stmt = conn
+                .prepare("INSERT INTO data (timestamp, bucket, payload) VALUES (?, ?, ?);")
+                .unwrap();
+            while let Some(p) = buffer_local.pop() {
+                stmt.execute(params![
+                    p.timestamp.unwrap_or(Utc::now().to_string()),
+                    p.bucket,
+                    p.payload.to_string(),
+                ])
+                .unwrap();
+            }
+            conn.execute_batch("COMMIT").unwrap();
+        }
+    }
+
+    let conn_clone = conn.clone();
+    let buffer_clone = buffer.clone();
+    task::spawn_blocking(move || task(conn_clone, buffer_clone));
+
+    let app = Router::new()
+        .route("/", post(upload_data))
+        .route("/gps", get(get_gps_coords))
+        .with_state((conn, buffer));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
+}
+
+#[derive(Debug, Serialize)]
+struct GPSResponse {
+    longitude: f64,
+    latitude: f64,
+}
+
+async fn get_gps_coords(
+    State((conn, _)): State<StateType>,
+) -> (StatusCode, Json<Vec<GPSResponse>>) {
+    let conn = conn.lock().await;
+    let mut stmt = conn
+        .prepare("SELECT cast(payload -> '$.longitude' as float), cast(payload -> '$.latitude' as float) FROM data WHERE bucket = 'location';")
+        .unwrap();
+
+    let response: Result<Vec<GPSResponse>, _> = stmt
+        .query_map([], |row| {
+            Ok(GPSResponse {
+                longitude: row.get(0)?,
+                latitude: row.get(1)?,
+            })
+        })
+        .unwrap()
+        .collect();
+
+    (StatusCode::OK, Json(response.unwrap()))
+}
+
+async fn upload_data(
+    State((_, buffer)): State<StateType>,
+    Json(payload): Json<Data>,
+) -> StatusCode {
+    let mut buffer = buffer.lock().await;
+
+    buffer.push(payload);
+
+    StatusCode::OK
+}
+
+// the input to our `create_user` handler
+#[derive(Deserialize, Clone)]
+struct Data {
+    timestamp: Option<String>,
+    bucket: String,
+    payload: Value,
 }
