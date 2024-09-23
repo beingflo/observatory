ca99e50bd1878fb7727e2bf1bdcd850a5befe33d
Florian Marending <florian@marending.dev>
2024-09-23 21:55:57 +0200

    [ui] Add favicon

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div class="text-xl font-bold p-8">Observatory</div>;
+  return (
+    <div>
+      <div class="text-xl font-bold p-8">Observatory</div>;
+    </div>
+  );
 }
 
 export default App;
