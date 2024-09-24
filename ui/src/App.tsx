c7bb2473008f73d8a8460a71bbee9dce70fb46a0
Florian Marending <florian@marending.dev>
2024-09-24 21:33:22 +0200

    [ui] Add weight display

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,29 @@
+import { createResource, For } from "solid-js";
+
+const fetchWeight = async () => {
+  const response = await fetch("/api/weight");
+  return response.json();
+};
+
 function App() {
+  const [data] = createResource(fetchWeight);
+
+  const formatter = new Intl.DateTimeFormat("en-CH", {
+    timeStyle: "medium",
+    dateStyle: "short",
+  });
+
   return (
     <div>
       <div class="text-xl font-bold p-8">Observatory</div>
+      <For each={data()}>
+        {(item) => (
+          <div class="flex flex-row gap-8 px-8">
+            <p>{formatter.format(new Date(item.timestamp))}</p>
+            <p>{item.weight}</p>
+          </div>
+        )}
+      </For>
     </div>
   );
 }
