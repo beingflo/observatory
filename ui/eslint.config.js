636caa14c368ff9a9743333c7e53be3abb95329b
Florian Marending <florian@marending.dev>
2024-09-24 21:26:02 +0200

    [ui] Tune down unused vars lint

--- ui/eslint.config.js
+++ ui/eslint.config.js
@@ -3,7 +3,12 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,tsx,jsx}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,tsx,jsx}"],
+    rules: {
+      "no-unused-vars": warn,
+    },
+  },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
