af8f2478637fc86e8d4e15db5fead6ce1320779c
Florian Marending <florian@marending.dev>
2024-11-03 19:22:17 +0100

    [ui] Add basic weight dashboard

--- ui/src/dashboards/Weight.tsx
+++ ui/src/dashboards/Weight.tsx
@@ -1,4 +1,5 @@
-import { createResource, For } from "solid-js";
+import { createResource, Show } from "solid-js";
+import * as Plot from "@observablehq/plot";
 
 const fetchWeight = async () => {
   const response = await fetch("/api/weight");
@@ -8,22 +9,33 @@ const fetchWeight = async () => {
 const Weight = () => {
   const [data] = createResource(fetchWeight);
 
-  const formatter = new Intl.DateTimeFormat("en-CH", {
-    timeStyle: "medium",
-    dateStyle: "short",
-  });
-
   return (
     <div>
       <div class="text-xl font-bold p-8">Weight</div>
-      <For each={data()}>
-        {(item) => (
-          <div class="flex flex-row gap-8 px-8">
-            <p>{formatter.format(new Date(item.timestamp))}</p>
-            <p>{item.weight}</p>
-          </div>
-        )}
-      </For>
+      <div class="max-w-2xl mx-auto mt-16">
+        <div class="p-2 flex flex-col gap-2 left-2 md:left-auto">
+          <Show when={data()}>
+            {Plot.plot({
+              y: {
+                grid: true,
+                domain: [90, 100],
+              },
+              marks: [
+                Plot.lineY(data()?.weights, {
+                  x: (d) => new Date(d.timestamp),
+                  y: "weight",
+                }),
+                Plot.lineY(data()?.smooth_weights, {
+                  x: (d) => new Date(d.timestamp),
+                  y: "weight",
+                  stroke: "red",
+                  opacity: 0.3,
+                }),
+              ],
+            })}
+          </Show>
+        </div>
+      </div>
     </div>
   );
 };
