89b7572673b083f503aba64b16c09e7e6f20d0ea
Florian Marending <florian@marending.dev>
2024-11-18 22:28:13 +0100

    [ui] Wire up date range selector for location dashboard

--- ui/src/dashboards/Location.tsx
+++ ui/src/dashboards/Location.tsx
@@ -1,19 +1,22 @@
 import { createMemo, createResource, createSignal } from "solid-js";
 import { Dashboard } from "../components/Dashboard";
 import MapGL, { Layer, Source, Viewport } from "solid-map-gl";
+import { useRange } from "../components/RangeProvider";
+import { getDate } from "../components/DateRangeSelector";
 
-const fetchData = async (_from?: string) => {
-  const response = await fetch(`/api/gps/location_florian`);
+const fetchData = async (from?: string) => {
+  const response = await fetch(`/api/gps/location_florian?from=${from}`);
   return response.json();
 };
 
 export const Location = () => {
+  const [{ from }] = useRange();
   const [viewport, setViewport] = createSignal({
     center: [8.53, 47.37],
     zoom: 13,
   } as Viewport);
 
-  const [data] = createResource(() => fetchData());
+  const [data] = createResource(from, () => fetchData(getDate(from())));
 
   const geoJson = createMemo(() => ({
     type: "Feature",
